f21652afe6e830dd3ecfa4ef3f2337c570772bdb
Ankit metho <166896147+Ankitmehto12@users.noreply.github.com>
2024-04-13 11:24:01 +0530

    Add files via upload

---
+++ -/folio.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.multitext ', {
+    // Waits 1000ms after typing "First"
+    strings: [' Ankit', ' coder',' developer'],
+    loop : true,
+    typespeed : 150,
+    backspeed : 150,
+    backdelay : 1500
+  });
